9fbf4fa6f62a2ac1cd44efc327abbce5e5ff7dee
ulisespallares888 <ulisespallares8@gmail.com>
2023-07-04 17:14:53 -0300

    COM3-Fix-NameAndLastName at Author

--- API/src/main/java/com/InfoSpring/API/controllers/impl/AuthorController.java
+++ API/src/main/java/com/InfoSpring/API/controllers/impl/AuthorController.java
@@ -6,10 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.actuate.autoconfigure.observation.ObservationProperties;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = "api/v1/author")
@@ -27,8 +24,8 @@ public class AuthorController extends BaseControllerImpl<Author, AuthorServiceIm
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("{\"error\": \"" + e.getMessage() + "\"}");
         }
     }
-    @GetMapping("/{nameAuthor}{lastNameAuthor}")
-    public ResponseEntity<?> findAuthorByNameAndLastName(@PathVariable("nameAuthor") String nameAuthor, @PathVariable("lastNameAuthor") String lastNameAuthor)  {
+    @GetMapping("/search")
+    public ResponseEntity<?> findAuthorByNameAndLastName(@RequestParam("nameAuthor") String nameAuthor, @RequestParam("lastNameAuthor") String lastNameAuthor)  {
         try {
             return ResponseEntity.status(HttpStatus.OK).body(authorService.findAuthorByNameAndLastName(nameAuthor,lastNameAuthor));
         } catch (Exception e){
